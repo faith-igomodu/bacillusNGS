f91ede660a92f4d1ce51ff2649fcc198b7f40e13
faith igomodu <figomodu@student.umgc.edu>
2026-05-07 20:41:58 -0400

    Initial commit

---
+++ gcconfigure/gcscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+# Script to provision cloud resources for genome annotation with Nextflow. 
+# Copyright (C) 2026 Faith Igomodu - All Rights Reserved 
+# Permission to copy and modify is granted under the Public license
+
+
+#Create project
+gcloud projects create bacillus-thuringensis 
+#Set project 
+gcloud config set project bacillus-thuringensis 
+#Assign project to a billing account 
+gcloud alpha billing projects link [PROJECT_ID] --billing-account=[BILLING_ACCOUNT_ID]
