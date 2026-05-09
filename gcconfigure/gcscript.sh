3892d39aef3b5806ec9b3118adf69fd7c1dded2b
faith <faith.gomo@outlook.com>
2026-05-08 20:48:27 -0400

    Update gcscript.sh

--- gcconfigure/gcscript.sh
+++ gcconfigure/gcscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 # Script to provision cloud resources for genome annotation with Nextflow. 
 # Copyright (C) 2026 Faith Igomodu - All Rights Reserved 
-# Permission to copy and modify is granted under the Public license
+# Permission to copy and modify is granted under the public license
 
 
 #Create project
